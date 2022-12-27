184fd5394ad653caf0cfcee14755029d372ae12e
Hafijul Islam <hafijul233@gmail.com>
2022-12-28 03:45:27 +0600

    conditions added

--- app/src/main/java/com/hafijulislam/tiptime/MainActivity.kt
+++ app/src/main/java/com/hafijulislam/tiptime/MainActivity.kt
@@ -2,10 +2,25 @@ package com.hafijulislam.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hafijulislam.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //if view view bind is enabled
+    lateinit var mainActivityBind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivityBind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivityBind.root)
+
+        mainActivityBind.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    fun calculateTip() {
+//        val costOfService = mainActivityBind.costOfService.text?.toDouble()
+
     }
 }
